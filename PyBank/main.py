630d0d27d4badf01a30ecc9a70e571801fcccd23
Anu Guha <aguha1@gmail.com>
2020-03-22 09:47:58 -0500

    PyBank_final.csv file created for output

--- PyBank/main.py
+++ PyBank/main.py
@@ -36,7 +36,28 @@ with open(PyBank_csv, encoding ='utf-8') as csvfile:
     for i in range(1,len(Profit_Loss2)):
         change_profit_loss.append(Profit_Loss2[i] - Profit_Loss2[i-1])
         average_change = sum(change_profit_loss)/len(change_profit_loss)
-        
+        greatest_profit = max(change_profit_loss)
+        greatest_loss =min(change_profit_loss)
     print(f'Average Change:${round(average_change,2)}')
-        #average= diff/2 
-        #print(f'{change_profit_loss}{average}')
+    
+    #Calculate the greatest increase in profit
+    print(f'Greatest increase in profit is ${greatest_profit}')
+
+    #Calculate the greatest decrease in Profit
+    print(f'Greatst decrease in profit is ${greatest_loss}')
+
+# Set variable for output file
+output_file = os.path.join("PyBank_final.csv")
+
+#  Open the output file
+with open(output_file, "w") as datafile:
+    writer = csv.writer(datafile)
+
+    # Write the rows
+    writer.writerow(['Financial Analysis'])
+    writer.writerow(['-----------------------'])
+    writer.writerow([f'Total Months: {len(Total_months)}'])
+    writer.writerow([f'Total: ${Sum}'])
+    writer.writerow([f'Average Change:${round(average_change,2)}'])
+    writer.writerow([f'Greatest increase in profit is ${greatest_profit}'])
+    writer.writerow([f'Greatst decrease in profit is ${greatest_loss}'])
